13729f4207777ede1c89980552bdb27223731dee
TheWangYang <wyysuccess2020@163.com>
2023-04-19 22:44:58 +0800

    Update this repo: add onnx in detection_process dir.

--- detection_process/picture_detection.py
+++ detection_process/picture_detection.py
@@ -23,6 +23,11 @@ def init_model():
     return model
 
 
+def pth2onnx():
+    model = init_model()
+    print("type(model): ", type(model))
+
+
 # 检测图片缺陷函数
 async def backend_detection_picture(request_data):
     print("request_data: {}".format(request_data))
@@ -42,7 +47,7 @@ async def backend_detection_picture(request_data):
     picture_detection_abs_save_path = PROJECT_PATH + "/static/images/done/"
     picture_detection_relative_save_path = "/static/images/done/"
     if os.path.exists(picture_abs_save_path) and picture_relative_save_path != "":
-        # todo-如果存在直接调用so动态库对图片进行推理并返回标注结果数组
+        # todo-如果存在直接调用dll动态库对图片进行推理并返回标注结果数组
         model = init_model()
         # 创建保存tmp对应的图片集合
         # 读取curr_picture_name对应的tmp文件夹下面的图片集合保存到数组中
@@ -54,7 +59,15 @@ async def backend_detection_picture(request_data):
             images_path_list.append(tmp_path + name)
         # detection_list
         detection_list = [images_path_list[1], images_path_list[2]]
-        result_list = inference_detector(model, detection_list)
+        # ------------------------------------------------------use onnx backend do inference-------------------------------------------------------
+        from mmdeploy.apis import inference_model
+        deploy_cfg = 'mmdeploy/configs/mmdet/detection/detection_onnxruntime_dynamic.py'
+        model_cfg = 'detection_process/configs/fcos_r50_caffe_fpn_gn-head_1x_spdfk_coco_3000_2000.py'
+        backend_files = ['detection_process/onnxs/fcos_end2end.onnx']
+        device = 'cpu'
+        result_list = inference_model(model_cfg, deploy_cfg, backend_files, detection_list, device)
+        # ------------------------------------------------------use onnx backend do inference-------------------------------------------------------
+        # result_list = inference_detector(model, detection_list)
         # print("result: {}".format(result))
         # 创建curr_name对应的tmp_result文件夹
         tmp_result = PROJECT_PATH + TEMP_IMAGES_RESULT + curr_picture_name[:-4] + "/"
@@ -101,3 +114,5 @@ async def backend_detection_picture(request_data):
     else:
         return [], os.path.join(picture_detection_relative_save_path,
                                 curr_picture_name)
+
+
